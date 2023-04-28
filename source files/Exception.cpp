30be579c102f9b6c03fbf1a90c9a4d0399b0f74b
victorstefan28 <116149427+victorstefan28@users.noreply.github.com>
2023-04-28 19:13:26 +0300

    extra ; :-1:

--- source files/Exception.cpp
+++ source files/Exception.cpp
@@ -4,16 +4,16 @@
 
 #include "../headers/headers.h"
 
-input_exception::input_exception(std::string msg_) : msg(msg_+'\n') {};
-input_exception::input_exception() : msg("Input Exception - No message\n") {};
+input_exception::input_exception(std::string msg_) : msg(msg_+'\n') {}
+input_exception::input_exception() : msg("Input Exception - No message\n") {}
 const char* input_exception::what() const throw()
 {
 
     return msg.c_str();
 }
 
-invalid_arg::invalid_arg(std::string msg_) : msg(msg_+'\n') {};
-invalid_arg::invalid_arg() : msg("Argument passed is invalid\n") {};
+invalid_arg::invalid_arg(std::string msg_) : msg(msg_+'\n') {}
+invalid_arg::invalid_arg() : msg("Argument passed is invalid\n") {}
 const char* invalid_arg::what() const throw()
 {
 
